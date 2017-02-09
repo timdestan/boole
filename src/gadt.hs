3f76042c9d4d513f7fbb2e392c75750cc861a6f7
Tim Destan <tim@destan.name>
2017-02-08 23:12:45 -0500

    Haskell GADT example

---
+++ src/gadt.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE GADTs #-}
+
+data Exp a where
+  I :: Int -> Exp Int
+  B :: Bool -> Exp Bool
+  Add :: Exp Int -> Exp Int -> Exp Int
+  Eq :: Exp Int -> Exp Int -> Exp Bool
+
+instance Show (Exp a) where
+  show (I i) = show i
+  show (B b) = show b
+  show (Add x y) = "(" ++ show x ++ " + " ++ show y ++ ")"
+  show (Eq x y) = show x ++ " = " ++ show y
+
+eval :: Exp a -> a
+eval (I i) = i
+eval (B b) = b
+eval (Add x y) = eval x + eval y
+eval (Eq x y) = eval x == eval y
+
+value = Eq (Add (I 4) (I 3)) (I 7)
+
+showResult :: Show a => Exp a -> IO ()
+showResult a = putStrLn $ show a ++ " -> " ++ show (eval a)
+
+main = do
+  showResult (Eq (Add (I 4) (I 3)) (I 7))
+  showResult (Eq (I 4) (I 3))
